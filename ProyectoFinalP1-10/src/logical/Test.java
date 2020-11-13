18452ef731cd01ccc971079b1479364731754a34
C.MARRERO <C.MARRERO@DESKTOP-0HLMP61>
2020-11-12 21:39:49 -0600

    Creacion del constructor y Getters n Setters

--- ProyectoFinalP1-10/src/logical/Test.java
+++ ProyectoFinalP1-10/src/logical/Test.java
@@ -2,6 +2,27 @@ package logical;
 
 public class Test {
 	private String prueba1;
-	
 	private String prueba2;
+	
+	public Test(String prueba1, String prueba2) {
+		super();
+		this.prueba1 = prueba1;
+		this.prueba2 = prueba2;
+	}
+
+	public String getPrueba1() {
+		return prueba1;
+	}
+
+	public void setPrueba1(String prueba1) {
+		this.prueba1 = prueba1;
+	}
+
+	public String getPrueba2() {
+		return prueba2;
+	}
+
+	public void setPrueba2(String prueba2) {
+		this.prueba2 = prueba2;
+	}
 }
